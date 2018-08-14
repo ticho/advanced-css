75e09c2a6568e76037bbf45f2ee7323d8e9c34fb
ticho <tibofolly@gmail.com>
2018-08-15 01:52:26 +0200

    readme link

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,19 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get root_path
     assert_response :success
+  end
+
+  test "presence of components" do
+    get root_path
+    assert_select '.navbar'
+    assert_select '.container'
+    assert_select '.wrapper'
+    assert_select '.tab'
+    assert_select '.button'
+    assert_select '.footer'
+    assert_select '.banner'
   end
 
 end
